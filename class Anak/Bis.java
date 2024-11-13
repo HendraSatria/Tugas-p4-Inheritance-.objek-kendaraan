b0200298bd71d9f5480871ad49129b647e723281
M hendra satria <162526343+HendraSatria@users.noreply.github.com>
2024-11-13 19:25:04 +0700

    Add files via upload

---
+++ class Anak/Bis.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Tugas4;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class Bis extends Kendaraan{
+    
+
+    void solar2(){
+        System.out.println("Bis Berjalan !!!");
+    }
+}
