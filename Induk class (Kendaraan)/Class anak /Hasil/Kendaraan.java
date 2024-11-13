f74764ab2c0cbd542f4d0a8de3b50c077e359981
M hendra satria <162526343+HendraSatria@users.noreply.github.com>
2024-11-13 19:23:33 +0700

    Add files via upload

---
+++ Induk class (Kendaraan)/Class anak /Hasil/Kendaraan.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Tugas4;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class Kendaraan {
+    String Merk;
+    String Platkendaraan;
+    String BahanBakar;
+    
+    
+    void maju(){
+        System.out.println("Kendaraan Berjalan");
+    
+    
+}
+}
