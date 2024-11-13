2342fb9c2071cdae791cfaa9716056cdb565be22
M hendra satria <162526343+HendraSatria@users.noreply.github.com>
2024-11-13 19:24:28 +0700

    Add files via upload

---
+++ class Anak/Motor.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Tugas4;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class Motor extends Kendaraan {
+    
+    
+    void pertalite  (){
+        System.out.println("SUPRA  Bapak Berjalan!!!");
+}
+}
